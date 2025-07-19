76406da340f321788dfb6e1d6551051f4eeaa188
wasabina67 <wasabina67@gmail.com>
2025-07-19 15:11:37 +0900

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -1,7 +1,23 @@
 Imports System
+Imports System.Text
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.OutputEncoding = Encoding.UTF8
+        Dim art As String() = {
+            " ***************************************** ",
+            " *                                       * ",
+            " *   Happy 40th Anniversary, Shoeisha!   * ",
+            " *                                       * ",
+            " ***************************************** "
+        }
+        For Each line As String In art
+            Console.WriteLine(line)
+        Next
+        Console.WriteLine(
+            vbCrLf &
+            " 翔泳社40周年、おめでとうございます！" & vbCrLf &
+            " これからも素晴らしい活動を応援しています！"
+        )
     End Sub
 End Module
